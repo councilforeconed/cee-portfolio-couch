895d06163febc56f125b424f25ae64328e013f90
Steve Kinney <hello@stevekinney.net>
2013-11-06 12:44:21 -0500

    Add in corpus of Common Core data

--- bin/update-ccss.js
+++ bin/update-ccss.js
@@ -1,10 +1,13 @@
 var db = require('../lib/couch-config');
 var ccss = require('../lib/ccss-alignments');
+var finder = require('../lib/common-core');
 
 db.view('common-core', 'lessons-with-ccss', function (err, body) {
   body.rows.forEach(function (doc) {
     var lesson = doc.value;
-    console.log(lesson);
+    lesson.commonCoreStandardIdentifiers.forEach(function (standard) {
+      console.log(finder(standard));
+    })
   });
 });
 
