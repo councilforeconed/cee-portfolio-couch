07b747c7d6cd698a61ddfcfb9ab911c6451e784b
Steve Kinney <hello@stevekinney.net>
2013-11-10 21:38:20 -0500

    Swap out Routie for Director

--- application/js/lib/routes.js
+++ application/js/lib/routes.js
@@ -1,142 +1,147 @@
 var $content = $('#main-content')
 var canvas = Handlebars.compile('<canvas id="{{id}}" width="' + $content.width() + '" height="' + $content.width()/2 +'"></canvas>')
 
-routie('/grades', function() {
-  api.get.grades(function (grades) {
-    if ( $('#grade-distribution').length === 0 ) {
-      $content.children().remove();
-      $content.prepend('<h3>Distribution of Content Across the Grades</h3>')
+var routes = {
+  '/grades': function() {
+    api.get.grades(function (grades) {
+      if ( $('#grade-distribution').length === 0 ) {
+        $content.children().remove();
+        $content.prepend('<h3>Distribution of Content Across the Grades</h3>')
       
-      var labels = _.map(grades, function (element) {
-        return element.key;
-      });
+        var labels = _.map(grades, function (element) {
+          return element.key;
+        });
 
-      var values = _.map(grades, function (element) {
-        return element.value;
-      });
+        var values = _.map(grades, function (element) {
+          return element.value;
+        });
 
-      var data = {
-    	  labels : labels,
-      	datasets : [ { fillColor : "rgba(202, 219, 42, 0.5)", strokeColor : "rgb(127, 181, 57)", data : values } ]
+        var data = {
+      	  labels : labels,
+        	datasets : [ { fillColor : "rgba(202, 219, 42, 0.5)", strokeColor : "rgb(127, 181, 57)", data : values } ]
+        }
+
+        var $canvas = $(canvas({id: 'grade-distribution'}));
+    
+        var ctx = $canvas.get(0).getContext("2d");
+        var chart = new Chart(ctx).Radar(data);
+        $content.append($canvas);
       }
+    });
+  },
 
-      var $canvas = $(canvas({id: 'grade-distribution'}));
-    
-      var ctx = $canvas.get(0).getContext("2d");
-      var chart = new Chart(ctx).Radar(data);
-      $content.append($canvas);
-    }
-  });
-});
+  '/formats': function() {
+    api.get.formats(function (formats) {
+      if ( $('#format-distribution').length === 0 ) {
+        $content.children().remove();
+      
+        var data = _.map(formats, function (row) { return { value: row.value } });
+      
+        data[0] = { color: util.color('light-blue'), value: data[0].value };
+        data[1] = { color: util.color('blue'), value: data[1].value };
 
-routie('/formats', function() {
-  api.get.formats(function (formats) {
-    if ( $('#format-distribution').length === 0 ) {
+        var $canvas = $(canvas({id: 'format-distribution'}));
+        var ctx = $canvas.get(0).getContext("2d");
+        var chart = new Chart(ctx).Pie(data);
+        $content.append($canvas);
+      }
+    });
+  },
+
+  '/publications': function() {
+    api.get.publications(function (publications) {
       $content.children().remove();
-      
-      var data = _.map(formats, function (row) { return { value: row.value } });
-      
-      data[0] = { color: util.color('light-blue'), value: data[0].value };
-      data[1] = { color: util.color('blue'), value: data[1].value };
+      $.get('templates/publications.hbs', function (template) {
+        var template = Handlebars.compile(template);
+        $content.append(template({publications: publications}));
+      })
+    });
+  },
 
-      var $canvas = $(canvas({id: 'format-distribution'}));
+  '/graph/:subject/:format': function(subject, format) {
+    api.get.standards(subject, format, function (standards) {
+      $content.children().remove();
+    
+      var color = ['light-blue', 'light-green'][['personal-finance', 'economics'].indexOf(subject)]
+    
+      var labels = _.map(standards, function (element) { return element.key; });
+      var values = _.map(standards, function (element) { return element.value; });
+      var data = { labels : labels, datasets : [util.applyColorsToData(color, { data : values })] };
+    
+      var $canvas = $(canvas({id: 'standards-distribution'}));
+    
       var ctx = $canvas.get(0).getContext("2d");
-      var chart = new Chart(ctx).Pie(data);
+      var distributionChart = new Chart(ctx).Bar(data, {
+        scaleOverride : true,
+        scaleSteps : 10,
+        scaleStepWidth : (function () { return Math.max.apply(this, values) / 10 })(),
+        scaleStartValue : 0,
+        scaleLabel : "<%=value%>"
+      });
       $content.append($canvas);
-    }
-  });
-});
-
-routie('/publications', function() {
-  api.get.publications(function (publications) {
-    $content.children().remove();
-    $.get('templates/publications.hbs', function (template) {
-      var template = Handlebars.compile(template);
-      $content.append(template({publications: publications}));
-    })
-  });
-});
-
-routie('/graph/:subject/:format', function(subject, format) {
-  api.get.standards(subject, format, function (standards) {
-    $content.children().remove();
-    
-    var color = ['light-blue', 'light-green'][['personal-finance', 'economics'].indexOf(subject)]
-    
-    var labels = _.map(standards, function (element) { return element.key; });
-    var values = _.map(standards, function (element) { return element.value; });
-    var data = { labels : labels, datasets : [util.applyColorsToData(color, { data : values })] };
-    
-    var $canvas = $(canvas({id: 'standards-distribution'}));
-    
-    var ctx = $canvas.get(0).getContext("2d");
-    var distributionChart = new Chart(ctx).Bar(data, {
-      scaleOverride : true,
-      scaleSteps : 10,
-      scaleStepWidth : (function () { return Math.max.apply(this, values) / 10 })(),
-      scaleStartValue : 0,
-      scaleLabel : "<%=value%>"
     });
-    $content.append($canvas);
-  });
-});
+  },
 
-routie('/table/:subject/:format', function (subject, format) {
-  api.get.standardsByGrade(subject, format, function (standards) {
-    $content.children().remove();
+  '/table/:subject/:format': function (subject, format) {
+    api.get.standardsByGrade(subject, format, function (standards) {
+      $content.children().remove();
     
-    standards.forEach(function (row) {
-      row.subject = subject;
-      row.format = format;
-    });
-    
-    $.get('templates/standards-table.hbs', function (template) {
-      
-      var template = Handlebars.compile(template)
-      $content.append(template({lessons: standards}));
-      
-      $('.standard-header').on('click', function (e) {
-        e.preventDefault();
-        var $this = $(this);
-        var standardNumber = $this.data('standard'); 
-        var subject = $this.data('subject');
-        
-        var standard = util.parseStandard(subject, standardNumber);
-      
-        $this.popover({
-          title: util.parseSubject(subject) + " Standard " + standardNumber,
-          content: '<strong>' + standard.topic + '</strong>: ' + standard.description,
-          html: true
-        })
+      standards.forEach(function (row) {
+        row.subject = subject;
+        row.format = format;
       });
-    });
-  });
-});
-
-routie('/subject/:subject/standard/:standard/grades/:grades/format/:format', function (subject, standard, grades, format) {
-  api.get.lessons(subject, standard, grades, format, function (lessons) {
-    $content.children().remove();
-    $.get('templates/lesson.hbs', function (resp) {
+    
+      $.get('templates/standards-table.hbs', function (template) {
       
-      var $lessons = $('<div class="lessons"></div>');
-      var template = Handlebars.compile(resp);
+        var template = Handlebars.compile(template)
+        $content.append(template({lessons: standards}));
       
-      var compiledLessons = _.each(lessons, function (lesson) {
-        $lessons.append(template(lesson.value));
-      });
-      
-      $lessons
-        .width($content.width())
-        .appendTo($content);
-      
-        $('.rating').on('click', function (e) {
+        $('.standard-header').on('click', function (e) {
           e.preventDefault();
           var $this = $(this);
-          var lesson = $this.data('lesson'); 
-          var rating = $this.data('rating');
-          
-          console.log(lesson, rating);
+          var standardNumber = $this.data('standard'); 
+          var subject = $this.data('subject');
+        
+          var standard = util.parseStandard(subject, standardNumber);
+      
+          $this.popover({
+            title: util.parseSubject(subject) + " Standard " + standardNumber,
+            content: '<strong>' + standard.topic + '</strong>: ' + standard.description,
+            html: true
+          })
         });
+      });
     });
-  });
-});
+  },
+
+  '/subject/:subject/standard/:standard/grades/:grades/format/:format': function (subject, standard, grades, format) {
+    api.get.lessons(subject, standard, grades, format, function (lessons) {
+      $content.children().remove();
+      $.get('templates/lesson.hbs', function (resp) {
+      
+        var $lessons = $('<div class="lessons"></div>');
+        var template = Handlebars.compile(resp);
+      
+        var compiledLessons = _.each(lessons, function (lesson) {
+          $lessons.append(template(lesson.value));
+        });
+      
+        $lessons
+          .width($content.width())
+          .appendTo($content);
+      
+          $('.rating').on('click', function (e) {
+            e.preventDefault();
+            var $this = $(this);
+            var lesson = $this.data('lesson'); 
+            var rating = $this.data('rating');
+          
+            console.log(lesson, rating);
+          });
+      });
+    });
+  }
+}
+
+var router = Router(routes);
+router.init(window.location.hash);
