4c8959db126ec97429405e435de0d6f85eadd2f4
Steve Kinney <hello@stevekinney.net>
2013-12-04 15:27:08 -0500

    Get labels correct

--- application/js/lib/controllers/lesson.js
+++ application/js/lib/controllers/lesson.js
@@ -1,5 +1,6 @@
 App.LessonController = Ember.ObjectController.extend({
   publicationID: function () {
     return this.get('model.publicationID') || 'econedlink';
-  }.property('model.publicationID')
+  }.property('model.publicationID'),
+  isOnline: Ember.computed.equal('format', 'online')
 });
--- application/js/lib/routes/publication.js
+++ application/js/lib/routes/publication.js
@@ -2,5 +2,11 @@ App.PublicationRoute = Ember.Route.extend({
   model: function (params) {
     if (params.publication_id === 'econedlink') params.publication_id = '\"econedlink\"';
     return Ember.$.getJSON('api/_design/publications/_list/lessons/lessons?start_key=' + params.publication_id + '&end_key=' + params.publication_id);
+  },
+  renderTemplate: function(controller, model) {
+    this.render('publication', { into: 'application', controller: controller });
+  },
+  setupController: function(controller, model) {
+    controller.set('model', model);
   }
 });
