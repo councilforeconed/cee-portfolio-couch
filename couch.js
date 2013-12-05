0957785f0402320419c417329d4543505c1b744e
Steve Kinney <hello@stevekinney.net>
2013-12-05 10:19:39 -0500

    Create multi-purpose search view

--- couch.js
+++ couch.js
@@ -40,29 +40,35 @@ design.views = {
       }
     }
   },
-  standard: {
-    map: function (doc) {
-      if (doc.portfolio) {
-        doc.economicsStandards.forEach(function (standard) {
-          emit(['economics', standard], doc);
-        });
-        doc.personalFinanceStandards.forEach(function (standard) {
-          emit(['personal-finance', standard], doc);
-        });
-        if (doc.commonCoreStandards) {
-          doc.commonCoreStandards.forEach(function (standard) {
-            emit(['common-core', standard], doc);
-          });
-        }
-      }
-    }
-  },
   concepts: {
     map: function (doc) {
       if (doc.portfolio && doc.concepts && doc.concepts.length) {
         doc.concepts.forEach(function (concept) {
           emit(concept, doc);
         });
+      }
+    }
+  },
+  standard: {
+    map: function () {
+      if (doc.portfolio) {
+        doc.economicsStandards.forEach(function (standard) {
+          doc.grades.forEach(function (grade) {
+            emit(['economics', standard, grade, doc.format, doc.type], doc);
+          });
+        });
+        doc.personalFinanceStandards.forEach(function (standard) {
+          doc.grades.forEach(function (grade) {
+            emit(['personal-finance', standard, grade, doc.format, doc.type], doc);
+          });
+        });
+        if (doc.commonCoreStandards) {
+          doc.commonCoreStandards.forEach(function (standard) {
+            doc.grades.forEach(function (grade) {
+              emit(['common-core', standard, grade, doc.format, doc.type], doc);
+            });
+          });
+        }
       }
     }
   }
