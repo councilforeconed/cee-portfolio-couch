533ad96a1cf9a428f4f04d7eb00542b856fa5bbf
Steve Kinney <hello@stevekinney.net>
2014-01-03 15:14:14 -0500

    Add standards to lesson template

--- application/javascripts/helpers/standard.js
+++ application/javascripts/helpers/standard.js
@@ -1,3 +1,4 @@
-Ember.Handlebars.helper('standardName', function(subject, standard) {
-  return standard.toString() + " - " + Portfolio.Standards.get(subject).get(standard - 1).topic;
+Ember.Handlebars.helper('standardName', function(subject, c, o) {
+  var standard = o.contexts[1]
+  return o.contexts[1] + ' - ' + Portfolio.Standards[subject][standard - 1].topic;
 });
